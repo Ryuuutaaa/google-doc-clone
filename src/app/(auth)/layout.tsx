5291dd456bf23ad082bfdf0c646b4428eb86d2c0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-27 01:13:35 +0800

    feat: replace DocumentLayout with AuthLayout for authentication pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <nav className="w-full bg-red-500">Auth Navbar navbar</nav>
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
