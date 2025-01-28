da910df8b69bb9c62317751ad37135bfc3f94a74
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-28 11:26:25 +0800

    feat: add toolbar component for document editing interface

---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const toolbar = () => {
+  return (
+    <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.2 overflow-auto ">
+      toolbar
+    </div>
+  );
+};
+
+export default toolbar;
