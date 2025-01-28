c02c797b10bff8dd0d28954951d714a78b9687b4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-28 11:52:59 +0800

    refactor: rename toolbar components for consistency and clarity

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,56 +1,54 @@
-import { cn } from "@/lib/utils";
-import { LucideIcon, Undo2Icon, UndoIcon } from "lucide-react";
-import React from "react";
+"use client";
 
-interface ToolbarButtonsProps {
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Undo2Icon, LucideIcon } from "lucide-react";
+
+interface ToolbarButtonProps {
+  icon: LucideIcon;
   onClick?: () => void;
   isActive?: boolean;
-  icon: LucideIcon;
 }
 
-const toolbarButton = ({
+const ToolbarButton = ({
+  icon: Icon,
   onClick,
   isActive,
-  icon: Icon,
-}: ToolbarButtonsProps) => {
-  return (
-    <button
-      onClick={onClick}
-      className={cn(
-        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
-        isActive && "bg-neutral-200/80"
-      )}
-    >
-      <Icon />
-    </button>
-  );
-};
+}: ToolbarButtonProps) => (
+  <button
+    onClick={onClick}
+    className={cn(
+      "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+      isActive && "bg-neutral-200/80"
+    )}
+  >
+    <Icon />
+  </button>
+);
 
-const toolbar = () => {
-  const sections: Array<
-    Array<{
-      label: string;
-      icon: LucideIcon;
-      onClick: () => void;
-      isActive: boolean;
-    }>
-  > = [
+const Toolbar = () => {
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
     [
       {
         label: "Undo",
         icon: Undo2Icon,
         onClick: () => console.log("Undo clicked"),
-        isActive: false,
       },
     ],
   ];
+
   return (
-    <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.2 overflow-auto ">
-      {sections[0].map((item) => {
-        <toolbarButton key={item.label} {...item} />;
-      })}
+    <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.2 overflow-auto">
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
     </div>
   );
 };
 
-export default toolbar;
+export default Toolbar;
