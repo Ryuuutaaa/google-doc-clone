0ded8a8bf7549364b37457ffa1ef316184a59814
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-28 11:34:44 +0800

    feat: implement toolbar button component for enhanced user interaction

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,4 +1,30 @@
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
 import React from "react";
+
+interface ToolbarButtonsProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+
+const toolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonsProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      <Icon />
+    </button>
+  );
+};
 
 const toolbar = () => {
   return (
