66ea04ea93184fa243646a6d0e0ae2bb8545f7d3
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-01 16:08:13 +0800

    feat: extend heading options in headingLeaveButton for improved text styling

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -29,7 +29,14 @@ import {
 const headingLeaveButton = () => {
   const { editor } = useEditorStore();
 
-  const headings = [{ label: "Normal text", value: 0, fontSize: "16px" }];
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
 };
 
 const FontFamilyButton = () => {
